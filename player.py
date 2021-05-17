6763aa730798151f8ec50cbb6c48794859f36475
AngelPillow <84335660+AngelPillow@users.noreply.github.com>
2021-05-17 20:38:29 +0300

    Update player.py

--- player.py
+++ player.py
@@ -125,9 +125,8 @@ class Player:
             return False
 
 
-
 class Enemy:
-    global eclass , eweapons , ehitpoints , earmourclass , edamage , edamagestr , emindmg , emaxdmg
+    global eclass , eweapons , ehitpoints , earmourclass , edamage , edamagestr , emindmg , emaxdmg , ename ,lichname , zombname , skelname
     eweapons = {'skeleton': 'dusty sword' , 'lich': 'witch bolt' , 'zombie': 'teeth and nails'}
     edamage = {'skeleton': d(3,8) , 'lich': d(2,12) , 'zombie': d(4,6)}
     edamagestr = {'skeleton': 'd(3,8)' , 'lich': 'd(2,12)' , 'zombie': 'd(4,6)}'}
@@ -135,6 +134,10 @@ class Enemy:
     ehitpoints = {'skeleton': 160 , 'lich': 140 , 'zombie': 150}
     emindmg =  {'skeleton': 3 , 'lich': 2 , 'zombie': 4}
     emaxdmg = {'skeleton': 24 , 'lich': 24 , 'zombie': 24}
+    zombname = ['Dancer', 'Gnawer', 'Frenzied', 'Blubber Zombie','Ripe Zombie','Clacker','Slumper','Mutant','Grower','Primer']
+    lichname = ["Cram'ghaic","Mhud'ghe","Chozgee","Kradredh","Vhoktag","Thoq'gac","Nanzagradh","Shum'zoudas","Dykragnuk","Thek'vagnudh"]
+    skelname = ["Naeryndam Herfir", "Thalanil Miranan", "Hagwin Balrieth", "Onas Virrel", "Aolis Enren","Braumseild", "Tyegnedrorr", "Lul"]
+    ename = zombname + lichname + skelname
 
     def __init__(self, eclass):
         self.eclass = eclass
@@ -145,21 +148,32 @@ class Enemy:
         self.emindmg = emindmg[self.eclass]
         self.emaxdmg = emaxdmg[self.eclass]
         self.edamagestr = edamagestr[self.eclass]
+        if eclass == 'zombie':
+            self.ename = zombname[randint(0,len(zombname))]
+        elif eclass == 'skeleton':
+            self.ename = skelname[randint(0,len(skelname))]
+        elif eclass == 'lich':
+            self.ename = lichname[randint(0,len(lichname))]
 
     def ecustom(self):
         self.eclass = input("Select your opponent's custom class ")
-        self.ehp = input("Select your opponent's HitPoint total (Integer)")
+        while True:
+            self.ename = input("Select your opponent's name , type [r] for random")
+            if self.ename.lower().strip() == 'r':
+                self.ename = ename[randint(0,len(ename))]
+                break
+        self.ehp = input(f"Select  {self.ename}'s HitPoint total (Integer)")
         while True:
             try:
                 int(self.ehp)
                 break
             except:
                 print('Enter a valid number ')
-                self.ehp = input("Select your opponent's HitPoint total (Integer)")
+                self.ehp = input(f"Select  {self.ename}'s HitPoint total (Integer)")
         self.ehp = int(self.ehp)
-        self.eweapon = input("Select your opponent's weapon ")
+        self.eweapon = input(f"Select  {self.ename}'s weapon ")
         while True:
-            self.eac = input("Select your ArmourClass total , min = 0 , max = 17.5 (can be float)")
+            self.eac = input(f"Select {self.ename}'s ArmourClass total , min = 0 , max = 17.5 (can be float)")
             try:
                 float(self.eac)
                 if 17.5 >= float(self.eac) >= 0:
@@ -170,7 +184,7 @@ class Enemy:
             except:
                 print('Invalid input , try again.')
         while True:
-            self.edamage = input("Select your opponent's damage type: [1].3d6 [2].2d12 [3].4d4 [i].info [c].Custom")
+            self.edamage = input(f"Select  {self.ename}'s damage type: [1].3d6 [2].2d12 [3].4d4 [i].info [c].Custom")
             if self.edamage.lower() == 'i':
                 print('XdY in tabletop rpg games means X dice with Y faces')
                 sleep(2)
@@ -209,7 +223,7 @@ class Enemy:
                     print('Invalid input , try again. ')
             else:
                 print('Enter a valid choice \n')
-                self.edamage = input("Select your opponent's damage type: [1].3d6 [2].2d12 [3].4d4 [i].Info [c].Custom")
+                self.edamage = input("Select  {self.ename}'s damage type: [1].3d6 [2].2d12 [3].4d4 [i].Info [c].Custom")
 
     def egetstats(self):
         self.estats = [self.eclass, self.ehp, self.eweapon,  self.eac, self.edamage]
@@ -247,81 +261,117 @@ class Enemy:
 
 
 def main():
-    global p1 , e1
-    start = input('Would you like to see the game intro? [Y] / [Type Anything]')
-    if start.upper() == 'Y':
-        intro()
-    defaultbattle()
-    pass
+    global p1 , e1 , name
+    intro()
+    name = input('Enter your name : ')
+    menu()
+
+def menu():
+    print('             ~~~|         Main menu        |~~~\n')
+    print('             ~~~|  [1]    Game Info        |~~~')
+    print('             ~~~|  [2]     Battle          |~~~')
+    print('             ~~~|  [3]      Quit           |~~~\n')
+    print('### Since this is a alpha version of the game , it may have inconsistencies ###\n')
+    enter = input('Type number to select')
+    if enter == '1':
+        info()
+    elif enter == '2':
+        print('             ~~~|           Battle Modes        |~~~\n')
+        print('             ~~~|   [1]    Classic Battle       |~~~')
+        print('             ~~~|   [2]     Custom Battle       |~~~')
+        print('             ~~~|   [3]         Back            |~~~\n')
+        print('### Please note that this game is not essentially balanced , since it has not been playtested ###\n')
+        reenter = input('Type number to select')
+        if reenter == '1':
+            defaultbattle()
+        elif reenter == '2':
+            custombattle()
+        elif reenter == '3':
+            print('You are now heading back to the main menu\n')
+            menu()
+        else:
+            print('Invalid choice.\n')
+            print('You are now heading back to the main menu\n')
+            menu()
+    else:
+        print('You are now exiting the game.\n')
 
 def info():
     print('This is an info guide on the battle mechanisms of "The Game" (Pending title).')
-    sleep(2)
+    sleep(1)
     print("Be sure to read this if you haven't played the game yet!")
-    sleep(2)
+    sleep(1)
     print('In this game the Player (you!) fights an opponent in a classic text-based rpg style.')
-    sleep(2)
+    sleep(1)
     print('The key traits of your character are: Class , HitPoints , Damage and Armour Class')
-    sleep(2)
+    sleep(1)
     while True:
-        know = input('For which asset do you want to know about? [class] / [hp] / [dmg] / [ac] / [modes] / [quit]')
+        know = input('For which asset do you want to know about? [class] / [hp] / [dmg] / [ac] / [modes] / [quit]\n')
         if know.lower().strip() == 'class':
-            print('''Character Class is the path your character has chosen in their life.
-                     In this pre Alpha version , in non-custom battles , your character can be 
-                     either a fighter , a wizard or a monk. In similar fashion , your ready-to-fight
-                     opponents have a limited variety of classes.\n Press anything to continue: ''')
+            print('''            Character Class is the path your character has chosen in their life.
+            In this pre Alpha version , in non-custom battles , your character can be
+            either a fighter , a wizard or a monk. In similar fashion , your ready-to-fight
+            opponents have a limited variety of classes.\n  ''' )
             print()
-            print('Note: Your class determines your damage input as well as your HitPoint total and ArmourClass!')
+            print('Note: Your class determines your damage input as well as your HitPoint total and ArmourClass!\n')
         elif know.lower().strip() == 'hp':
-            print('''Your HitPoint total represents your ability to survive damage in battle.
-                     The more HitPoints you have , the harder it is for you to be dealt lethal
-                     damage from a hit and die.\n Press anything to continue: ''')
+            print('''            Your HitPoint total represents your ability to survive damage in battle.
+            The more HitPoints you have , the harder it is for you to be dealt lethal
+            damage from a hit and die.\n  ''')
             print()
-            print('Note: Your HitPoint total varies for different character classes!')
+            print('Note: Your HitPoint total varies for different character classes!\n')
         elif know.lower().strip() == 'dmg':
-            print('''The term damage represents the lethality of your hits versus your opponent's 
-                     HitPoint total. You can deal damage to your opponent by selecting the [Attack]
-                     action in-battle and vise-versa. The [Focus] action makes your future attacks 
-                     deal more damage. \n Press anything to continue: ''')
+            print('''            The term damage represents the lethality of your hits versus your opponent's 
+            HitPoint total. You can deal damage to your opponent by selecting the [Attack]
+            action in-battle and vise-versa. The [Focus] action makes your future attacks 
+            deal more damage. \n  ''')
             print()
-            print('Note: Your damage varies for different character classes!')
+            print('Note: Your damage varies for different character classes!\n')
         elif know.lower().strip() == 'ac':
-            print('''Your ArmourClass score represents your ability to not be hit or affected by
-                     an attack. When attacking or being attacked , a 20-sided dice is rolled and if that roll is 
-                     equal or greater than your AC , the attack hits! In the special case of a 20 ,
-                     the attack hits and deals two times the damage it would normally deal.
-                     Your (and your enemy's) AC can be amplified by the [Prepare] action in-battle
-                     to make it harder for your opponent to hit you (and vise-versa).\n Press anything to continue: ''')
+            print('''            Your ArmourClass score represents your ability to not be hit or affected by
+            an attack. When attacking or being attacked , a 20-sided dice is rolled and if that roll is 
+            equal or greater than your AC , the attack hits! In the special case of a 20 ,
+            the attack hits and deals two times the damage it would normally deal.
+            Your (and your enemy's) AC can be amplified by the [Prepare] action in-battle
+            to make it harder for your opponent to hit you (and vise-versa).\n  ''')
             print()
-            print('Note: Your AC varies for different classes!')
+            print('Note: Your AC varies for different classes!\n')
         elif know.lower().strip() == 'modes':
-            print('''The modes in this pre Alpha version are: Classic & Custom.
-                     In Classic , you may choose to be a character with pre determined stats
-                     that fights a similarly determined opponent.
-                     In Custom you may choose to be a character made completely out of your imagination
-                     (some restrictions are applied) and battle either a pre-determined opponent or create
-                     one of your own. For the time being , you cannot save your own characters or opponents.
-                     Press anything to continue: ''')
+            print('''            The modes in this pre Alpha version are: Classic & Custom.
+            In Classic , you may choose to be a character with pre determined stats
+            that fights a similarly determined opponent.
+            In Custom you may choose to be a character made completely out of your imagination
+            (some restrictions are applied) and battle either a pre-determined opponent or create
+            one of your own. For the time being , you cannot save your own characters or opponents.\n  ''')
             print()
-            print('Note: This took way longer than expected.')
-
-
-
+            print('Note: This took way longer than expected...\n')
+        elif know.lower().strip() == 'quit':
+            print('You have now exited the info menu.\n')
+            menu()
+            break
+        else:
+            print("That asset doesn't exist / Invalid choice.")
+            print('You are heading back to the main menu\n')
+            info()
 
 def intro():
     print('Welcome to the pre Alpha version of "The Game" (Pending title).')
     sleep(2)
     print('This is a simulator for text-based rpg style battles')
     sleep(2)
-    print('You may choose to be one of the listed:')
+    print('Three different classes await you... Three different ways to play the game.')
     sleep(2)
-    print('Fighter , Wizard or Monk')
+    print('Fighter')
+    sleep(1)
+    print('           Wizard')
+    sleep(1)
+    print('                      Monk')
+    sleep(1)
+    print('The fighter uses their sword and bulky defence to outlast their opponent.')
     sleep(2)
-    print('The fighter uses their sword and bulky defence to outlast their opponent')
+    print('The wizard specializes in destructive spells , while being relatively easy to be taken down.')
     sleep(2)
-    print('The wizard specializes in destructive spells , while being relatively easy to be taken down')
-    sleep(2)
-    print('Last but not least , the monk uses their own hands to knock out their opponent')
+    print('Last but not least , the monk uses their own hands to knock out their opponent.')
     sleep(2)
     print('What kind of opponents are you ready to face? Because they are lurking . . .\n')
 
@@ -346,6 +396,7 @@ def select():
     char = input('Type the name of your desired class\n')
     while True:
         if char == 'fighter' or char == 'wizard' or char == 'monk':
+            print(f'So you are {name} the {char}! Well then!')
             break
         else:
             print('Enter a valid class')
@@ -381,7 +432,7 @@ def moveoutcome(player,enemy):
             if roll == 20:
                 print('### Critical Hit ###\n')
                 dmg = 2 * player.damage
-                print(f'You dealt a total of {dmg} points of damage to your opponent!')
+                print(f'You dealt a total of {dmg} points of damage to  {enemy.ename}!')
                 enemy.ehp -= dmg
             else:
                 dmg = player.damage
@@ -391,7 +442,7 @@ def moveoutcome(player,enemy):
             print("The attack doesn't hit...\n")
     elif mymove == 2:
         if player.ac >= 15:
-            print('You cannot make it harder for your opponent to hit you...')
+            print(f'You cannot make it harder for  {enemy.ename} to hit you...')
         else:
             c[1] += 1
             ch = 2
@@ -417,28 +468,28 @@ def emoveoutcome(player,enemy):
             if eroll == 20:
                 print('### Critical Hit ###\n')
                 edmg = 2 * enemy.edamage
-                print(f'Your opponent dealt {edmg} points of damage to you!')
+                print(f' {enemy.ename} dealt {edmg} points of damage to you!')
                 player.hp -= edmg
             else:
                 edmg = enemy.edamage
-                print(f'Your opponent dealt {edmg} points of damage to you!')
+                print(f' {enemy.ename} dealt {edmg} points of damage to you!')
                 player.hp -= edmg
         else:
             print("The attack doesn't hit...\n")
     elif theirmove == 2:
         if enemy.eac >= 17.5:
-            print("Your opponent cannot make it any harder for you to hit")
+            print(" {enemy.ename} cannot make it any harder for you to hit")
         else:
-            print('It will be now harder to hit your opponent!\n')
+            print(f'It will be now harder to hit  {enemy.ename}!\n')
             enemy.eac += 0.5
     elif theirmove == 3:
-        print("Your opponent's next attacks will be more devastating!\n")
+        print(f"{enemy.ename}'s next attacks will be more devastating!\n")
         enemy.edamage += d(1,10)
         enemy.emindmg += 1
         enemy.emaxdmg += 10
 
 def showdown(player,enemy):
-    print(f'The battle between you and the opposing {enemy.eclass} have begun!\n')
+    print(f'The battle between you and the opposing {enemy.ename} , the {enemy.eclass} have begun!\n')
     while True:
         if player.alive() and enemy.ealive():
             moveoutcome(player,enemy)
@@ -449,6 +500,14 @@ def showdown(player,enemy):
         elif not player.alive():
             print('~~~You have died~~~')
             break
-        print(f"~~~ Your HP : [{player.hp}] ~~~  VS  ~~~Your opponent's HP : [{enemy.ehp}] ~~~\n")
+        print(f"~~~ Your HP : [{player.hp}] ~~~  VS  ~~~ {enemy.ename}'s HP : [{enemy.ehp}] ~~~\n")
         print(f'~~~ Chance of landing an attack : {(20-enemy.eac)*5}% ~~~  VS  ~~~ Chance of being hit by an attack : {(20-player.ac)*5}% ~~~\n')
         print(f"~~~ Expected damage from your attacks : {player.mindmg} - {player.maxdmg}~~~  VS  ~~~ Expected damage from opponent's attacks : {enemy.emindmg} - {enemy.emaxdmg}\n")
+    replay = input('Replay? [Y] / [N]')
+    if replay.lower().strip() == 'y':
+        showdown(player,enemy)
+    else:
+        menu()
+
+
+menu()
